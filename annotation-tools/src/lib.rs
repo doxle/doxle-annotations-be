4244bb7e50e524bd49de943ca82af540808fb9c5
doxle <design@doxle.com>
2025-10-10 11:17:01 +0530

    Add canvas-bevy and annotation-tools for desktop/mobile reuse

---
+++ annotation-tools/src/lib.rs
@@ -0,0 +1,2 @@
+// Annotation tools (polygon, bbox, brush)
+// TODO: Implement annotation logic and geometry calculations
---
+++ canvas-bevy/src/lib.rs
@@ -0,0 +1,2 @@
+// Bevy 2D renderer for desktop/mobile
+// TODO: Implement Bevy rendering systems
